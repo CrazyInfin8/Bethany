63868747d5ef7a60d46014017f75aa70a4a9ff7c
CrazyInfin8 <Crazyinfin8@yahoo.com>
2020-11-25 19:38:04 -0800

    Initial Commit

---
+++ Bethany/src/main/java/com/crazyinfin8/bethany/Bot.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.crazyinfin8.bethany;
+
+/**
+ *
+ * @author CrazyInfin8
+ */
+public class Bot {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
