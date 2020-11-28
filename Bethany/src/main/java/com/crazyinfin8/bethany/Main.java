2c867f6fcc4948d14bea721c64c8f4642951c3bc
CrazyInfin8 <Crazyinfin8@yahoo.com>
2020-11-28 02:05:20 -0800

    Poll command now tallys and displays results

--- Bethany/src/main/java/com/crazyinfin8/bethany/Main.java
+++ Bethany/src/main/java/com/crazyinfin8/bethany/Main.java
@@ -1,6 +1,9 @@
 package com.crazyinfin8.bethany;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import javax.security.auth.login.LoginException;
 import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.EmbedBuilder;
@@ -15,7 +18,7 @@ public class Main {
     public static void main(String[] args) throws LoginException, IOException {
         Dotenv dotenv = Dotenv.load();
         String token = dotenv.get("TOKEN");
-        if(token == null) {
+        if (token == null) {
             System.out.println("No token specified in \".env\" file.");
             return;
         }
@@ -39,10 +42,11 @@ class Poll implements Command {
             ":regional_indicator_o:", ":regional_indicator_p:", ":regional_indicator_q:", ":regional_indicator_r:",
             ":regional_indicator_s:", ":regional_indicator_t:", ":regional_indicator_u:", ":regional_indicator_v:",
             ":regional_indicator_w:", ":regional_indicator_x:", ":regional_indicator_y:", ":regional_indicator_z:", };
-    private final String EMOTE_CODES[] = new String[] { "U+1F1E6", "U+1F1E7", "U+1F1E8", "U+1F1E9", "U+1F1EA",
-            "U+1F1EB", "U+1F1EC", "U+1F1ED", "U+1F1EE", "U+1F1EF", "U+1F1F0", "U+1F1F1", "U+1F1F2", "U+1F1F3",
-            "U+1F1F4", "U+1F1F5", "U+1F1F6", "U+1F1F7", "U+1F1F8", "U+1F1F9", "U+1F1FA", "U+1F1FB", "U+1F1FC",
-            "U+1F1FD", "U+1F1FE", "U+1F1FF", };
+    private final String EMOTE_CODES[] = new String[] { "\uD83C\uDDE6", "\uD83C\uDDE7", "\uD83C\uDDE8", "\uD83C\uDDE9",
+            "\uD83C\uDDEA", "\uD83C\uDDEB", "\uD83C\uDDEC", "\uD83C\uDDED", "\uD83C\uDDEE", "\uD83C\uDDEF",
+            "\uD83C\uDDF0", "\uD83C\uDDF1", "\uD83C\uDDF2", "\uD83C\uDDF3", "\uD83C\uDDF4", "\uD83C\uDDF5",
+            "\uD83C\uDDF6", "\uD83C\uDDF7", "\uD83C\uDDF8", "\uD83C\uDDF9", "\uD83C\uDDFA", "\uD83C\uDDFB",
+            "\uD83C\uDDFC", "\uD83C\uDDFD", "\uD83C\uDDFE", "\uD83C\uDDFF", };
 
     public void run(Bot bot, JDA jda, Message msg, String... params) {
         EmbedBuilder emb = new EmbedBuilder();
@@ -55,17 +59,37 @@ class Poll implements Command {
             return;
         }
         int count = 1;
-        emb.appendDescription("**" + params[0] + "**\n");
+        emb.appendDescription("**New poll:**\n" + params[0] + "\n");
         for (; count < params.length && count < EMOTES.length; ++count) {
             emb.appendDescription(EMOTES[count - 1] + " ` " + params[count] + " `\n");
         }
         final int optCount = count - 1;
-        msg.getChannel().sendMessage(emb.build()).queue(newMsg -> {
-            for (int i = 0; i < optCount; ++i) {
-                newMsg.addReaction(EMOTE_CODES[i]).queue();
+        Message newMsg = msg.getChannel().sendMessage(emb.build()).complete();
+        for (int i = 0; i < optCount; ++i) {
+            newMsg.addReaction(EMOTE_CODES[i]).complete();
+        }
+        new Timer().schedule(new Task(() -> {
+            EmbedBuilder results = new EmbedBuilder();
+            results.appendDescription("**Pole results:**\n");
+            for (int i = 0; i < optCount; i++) {
+                // TODO: Make this output look a bit more pretty!
+                results.appendDescription(params[i + 1] + ": `"
+                        + (newMsg.retrieveReactionUsers(EMOTE_CODES[i]).complete().size() - 1) + "`\n");
             }
-            // TODO: Tally commands after a certain amount of time and update message to
-            // display results.
-        });
+            newMsg.clearReactions().complete();
+            newMsg.editMessage(results.build()).complete();
+        }), 5000);
+    }
+}
+
+class Task extends TimerTask {
+    Runnable fn;
+
+    public Task(Runnable fn) {
+        this.fn = fn;
+    }
+
+    public void run() {
+        fn.run();
     }
 }
